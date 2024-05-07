0c50092664d1ec287f5e649156c13269a34638cc
RaulGM3 <apaladario@gmail.com>
2024-05-07 18:19:06 -0400

    assigment 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,36 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { GameControlComponent } from './game-control/game-control.component';
+import { ControllerService } from './controller.service';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, GameControlComponent, OddComponent, EvenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  logArray = [new Date ()];
-  showSecret = true;
 
-  constructor () {}
+  showSecret = true;
+  ref: any;
+  array: number [] = [];
+
+  constructor (
+    private log: ControllerService
+  ) {}
+
+  ngOnInit () {
+    this.log.log.subscribe ((array: number []) => {
+      this.array = array;
+      console.log (this.array)
+    });
+  }
 
   onDisplayDetails () {
-    this.showSecret = !this.showSecret;
-    this.logArray.push(new Date());
+    clearInterval (this.ref);
   }
 }
---
+++ src/app/even/even.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-even',
+  standalone: true,
+  imports: [],
+  templateUrl: './even.component.html',
+  styleUrl: './even.component.scss'
+})
+export class EvenComponent {
+  @Input() number: number = 0;
+
+  constructor() {}
+
+  ngOnInit () {
+    console.log (this.number)
+  }
+}
