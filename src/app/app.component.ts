82121b4e0a41dffb532656fa96fce802df0a3fe3
RaulGM3 <apaladario@gmail.com>
2024-05-07 12:24:26 -0400

    assigment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SuccessComponent, WarningComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
